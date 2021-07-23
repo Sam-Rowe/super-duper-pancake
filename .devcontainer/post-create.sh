da72bf71e25ed1ee4fa39efae7649d801b876f7c
Samuel Rowe <14168597+Sam-Rowe@users.noreply.github.com>
2021-07-23 10:24:30 +0000

    Updated to include az cli and azure functions tools

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -10,3 +10,24 @@ fi
 if [ -f Gemfile ]; then
     bundle install
 fi
+
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-bionic-prod bionic main" > /etc/apt/sources.list.d/dotnetdev.list'
+
+sudo apt-get update
+
+sudo apt-get update
+sudo apt-get install azure-functions-core-tools-3
+
+wget https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
+
+sudo apt-get update; \
+  sudo apt-get install -y apt-transport-https && \
+  sudo apt-get update && \
+  sudo apt-get install -y dotnet-sdk-5.0
