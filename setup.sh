f07d00873683c26c85b5c109ee9f25d4e7b1b130
Samuel Rowe <14168597+Sam-Rowe@users.noreply.github.com>
2021-07-28 12:26:06 +0000

    Small steps to making codespace work with forwarded ports

--- setup.sh
+++ setup.sh
@@ -3,3 +3,10 @@
 # Sorry not sorry if this annoys you. I'm just trying to make it easy for me as I keep forgetting the 3.
 echo "alias pip=pip3" >> ~/.bashrc
 echo "alias pip=pip3" >> ~/.zshrc
+echo "alias get-func-add='npx -q codespaces-port 7071'" >> ~/.zshrc
+echo "alias get-site-add='npx -q codespaces-port 4000'" >> ~/.zshrc
+
+export base_url=`get-site-add`
+export api_url=`get-func-add`
+
+sed -i "s|^url: \".*\"|url: \"$base_url\"|g" /workspaces/$RepositoryName/site/rad/_config.codesapce.yml
