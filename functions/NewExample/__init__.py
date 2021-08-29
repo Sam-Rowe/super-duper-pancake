54800cc54fd60df0df850825f1f6923f6e680cf7
Samuel Rowe <14168597+Sam-Rowe@users.noreply.github.com>
2021-08-29 08:17:52 +0000

    Make colours colourblind sife

--- functions/NewExample/__init__.py
+++ functions/NewExample/__init__.py
@@ -9,6 +9,9 @@ import azure.functions as func
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
+    # Colour blind colour list ( IBM colour list ) from https://davidmathlogic.com/colorblind/#%23648FFF-%23785EF0-%23DC267F-%23FE6100-%23FFB000
+    colour = np.array(["#648FFF","#785EF0","#DC267F","#FE6100","#FFB000"])
+
     # using the requests libary get the regional carbon data from https://api.carbonintensity.org.uk/regional/england/ and return as a json string
     r = requests.get('https://api.carbonintensity.org.uk/regional/england/')
     
@@ -20,17 +23,28 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     # for each mix object in mix_json append fuel to  fuels np.array and perc to  percentages np.array
     fuels = np.array([])
     percentages = np.array([])
+    colours = np.array([])
     for mix in mix_json:
         # log mix
         logging.info(mix)
-        fuels = np.append(fuels, mix['fuel'])
-        percentages = np.append(percentages, mix['perc'])
-    
+        # if perc is not 0 append fuel to  fuels np.array and perc to  percentages np.array
+        if mix['perc'] != 0:
+            # log fuel
+            logging.info(mix['fuel'])
+            fuels = np.append(fuels, mix['fuel'].title())
+            percentages = np.append(percentages, mix['perc'])
+            # log fuel index in np.array
+            logging.info(fuels.size)
+            # use fuels.size modulus divided by size of colour array to index colour array
+            colours = np.append(colours, colour[fuels.size % colour.size])
+
+        
     # use fuels and percentages to create a pie chart using the matplotlib pyplot pie library
     # export pie chart to jpg file
     # return jpg file as a response
     fig, ax = matplotlib.pyplot.subplots()
-    ax.pie(percentages, labels=fuels, autopct='%1.1f%%', shadow=True, startangle=90)
+    ax.pie(percentages, labels=fuels, autopct='%1.1f%%', shadow=True, startangle=90, colors=colours)
+    #ax.pie(percentages, labels=fuels, autopct='%1.1f%%', shadow=True, startangle=90)
     matplotlib.pyplot.axis('equal')
     matplotlib.pyplot.savefig('mypie.jpg')
     matplotlib.pyplot.close()
